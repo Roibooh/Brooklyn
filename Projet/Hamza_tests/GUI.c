6f13962366232afc5cc567ab13541ee073fe9279
OHamza <55755676+OHamza@users.noreply.github.com>
2022-04-03 17:21:01 -0400

    test
    crash test

---
+++ Projet/Hamza_tests/GUI.c
@@ -0,0 +1,46 @@
+//gcc  -Wall -Wextra -o GUI GUI.c $(pkg-config gtk+-3.0 --cflags --libs) -rdynamic
+#include <gtk/gtk.h>
+
+
+void quitButtonClicked(__attribute__((unused)) GtkWidget *widget, 
+                       __attribute__((unused)) gpointer   data)
+{
+    g_print("Quit Clicked\n");
+    gtk_main_quit();
+}
+
+
+// Handle the user trying to close the window 
+gboolean windowDelete(__attribute__((unused)) GtkWidget *widget, 
+                      __attribute__((unused)) GdkEvent  *event,
+                      __attribute__((unused)) gpointer   data)
+{
+    g_print("%s called.\n",__FUNCTION__);
+    return TRUE;    // Returning TRUE stops the window being deleted.
+                    // Returning FALSE allows deletion.   
+}
+
+int main ( int argc, char **argv) {
+ 
+    GtkWidget *window;
+    GtkBuilder *builder = NULL;
+ 
+    gtk_init (&argc , &argv);
+ 
+    builder = gtk_builder_new();
+ 
+    if( gtk_builder_add_from_file (builder,"GUI.glade" , NULL) == 0)
+    {
+        printf("gtk_builder_add_from_file FAILED\n");
+        return(0);
+    }
+ 
+    window  = GTK_WIDGET (gtk_builder_get_object (builder,"window1"));
+   
+ 
+    gtk_builder_connect_signals(builder,NULL);
+ 
+    gtk_widget_show_all (window);
+    gtk_main ();
+    return 0;
+}
