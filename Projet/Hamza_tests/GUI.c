23224849e0a5aa93bf4396452ffb55995dc7ff68
OHamza <55755676+OHamza@users.noreply.github.com>
2022-04-04 10:48:21 -0400

    fixed interface depart arrivee calcul
    ajout des boutons départ et arrivée et le lanceur

--- Projet/Hamza_tests/GUI.c
+++ Projet/Hamza_tests/GUI.c
@@ -1,6 +1,25 @@
 //gcc  -Wall -Wextra -o GUI GUI.c $(pkg-config gtk+-3.0 --cflags --libs) -rdynamic
 #include <gtk/gtk.h>
 
+static GtkWidget *depart;
+static GtkWidget *arrivee;
+static GtkWidget *lanceur;
+static GtkWidget *result;
+
+
+void calculate(__attribute__((unused)) GtkWidget *widget, 
+               __attribute__((unused)) gpointer   data)
+{
+    int num1 = atoi((char *)gtk_entry_get_text(GTK_ENTRY(depart)));
+    int num2 = atoi((char *)gtk_entry_get_text(GTK_ENTRY(arrivee)));
+    
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "%d", num1 + num2);
+    
+    gtk_label_set_text(GTK_LABEL(result), buffer);
+    
+}
+
 
 void quitButtonClicked(__attribute__((unused)) GtkWidget *widget, 
                        __attribute__((unused)) gpointer   data)
@@ -39,6 +58,14 @@ int main ( int argc, char **argv) {
    
  
     gtk_builder_connect_signals(builder,NULL);
+    
+    depart = GTK_WIDGET(gtk_builder_get_object(builder, "depart"));
+    arrivee = GTK_WIDGET(gtk_builder_get_object(builder, "arrivee"));
+    lanceur = GTK_WIDGET(gtk_builder_get_object(builder, "lanceur"));
+    result = GTK_WIDGET(gtk_builder_get_object(builder, "result"));
+    
+    g_signal_connect(lanceur, "clicked", G_CALLBACK(calculate), NULL);
+    
  
     gtk_widget_show_all (window);
     gtk_main ();
