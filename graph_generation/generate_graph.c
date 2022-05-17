ef25a77770557bd6f0ea141faf8837c5eac9d9b1
TitouanLocatelli <55802446+TitouanLocatelli@users.noreply.github.com>
2022-05-17 11:27:51 +0200

    graph gen folder

---
+++ graph_generation/generate_graph.c
@@ -0,0 +1,39 @@
+#include "generate_bus.h"
+#include "generate_tram.h"
+#include "generate_metro.h"
+#include "generate_bike.h"
+#include "generate_walk.h"
+#include "graph_print.h"
+
+
+#define BIKE_WEIGHT 30
+#define METRO_WEIGHT 6
+#define TRAM_WEIGHT 18
+#define BUS_WEIGHT 24
+#define NORTH 0
+#define EAST 1
+#define SOUTH 2
+#define WEST 3
+
+
+void generate_graph(int height, int width)
+{
+    /*int * buses = generate_bus(height, width);
+    int counter = 0;
+    while(*(buses+counter) != -1)
+    {
+        printf("%i\n", *(buses+counter));
+        counter++;
+    }
+    print_generated_motorized_graph(height, width, buses, BUS);*/
+    //create_walk_graph(height, width);
+    //int bike_count = generate_bike(height, width);
+    //print_generated_bike_graph(21, 21, bike_count);
+    create_walk_graph(height, width);
+    int * metro = generate_metro_v1(height, width); /////////////////////////////DEBUG AT 24*40 seg fault
+    int * tram = generate_tram(height, width);
+    print_generated_motorized_graph(height, width, metro, METRO);
+    print_generated_motorized_graph(height, width, tram, TRAM);
+    //int * trams = generate_tram(height, width);
+    //print_generated_tram_graph(height, width, trams);
+}
---
+++ graph_generation/generate_graph.h
@@ -0,0 +1,6 @@
+#ifndef GENERATE_GRAPH_H
+#define GENERATE_GRAPH_H
+
+int generate_graph(int height, int width);
+
+#endif
